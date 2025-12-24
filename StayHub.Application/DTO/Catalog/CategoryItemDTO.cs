56f3c4412b6f0a575b5134159e9fb60c8ecdd119
PHONGPC\minhp <minhphonghp2003@gmail.com>
2025-12-24 08:10:05 +0700

    ...

--- StayHub.Application/DTO/Catalog/CategoryItemDTO.cs
+++ StayHub.Application/DTO/Catalog/CategoryItemDTO.cs
@@ -10,6 +10,7 @@ namespace StayHub.Application.DTO.Catalog
     {
         public string Name { get; set; }
         public string? Code { get; set; }
+        public string? Value { get; set; }
         public string? Description { get; set; }
     }
 }
