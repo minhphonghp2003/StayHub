c6ed6a95101a030708b67358a97c7811e4d1a109
PHONGPC\minhp <minhphonghp2003@gmail.com>
2026-01-04 21:09:20 +0700

    assign action and role

---
+++ StayHub.Application/CQRS/RBAC/Command/RoleAction/AssignActionsToRoleCommand.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Shared.Response;
+using StayHub.Application.DTO.RBAC;
+using StayHub.Application.Interfaces.Repository.RBAC;
+
+namespace StayHub.Application.CQRS.RBAC.Command.MenuAction
+{
+    // Include properties to be used as input for the command
+    public record AssignActionsToRoleCommand(List<int> actionIds, int roleId) : IRequest<Response<List<int>>>;
+    public sealed class AssignActionsToRoleCommandHandler(IRoleActionRepository roleActionRepository) : BaseResponseHandler, IRequestHandler<AssignActionsToRoleCommand, Response<List<int>>>
+    {
+        public async Task<Response<List<int>>> Handle(AssignActionsToRoleCommand request, CancellationToken cancellationToken)
+        {
+            var existActions = (await roleActionRepository.GetManyAsync(ma => ma.RoleId == request.roleId && request.actionIds.Contains(ma.ActionId),
+                selector: (e, i) => e.ActionId)).ToList();
+            var newActionIds = request.actionIds.Except(existActions).ToList();
+            var result = await roleActionRepository.AddRangeAsync(newActionIds.Select(aid => new StayHub.Domain.Entity.RBAC.RoleAction
+            {
+                ActionId = aid,
+                RoleId = request.roleId
+            }).ToList());
+
+            if (result == null)
+                return Failure<List<int>>("Failed to assign actions to role.", System.Net.HttpStatusCode.OK);
+
+            return Success(result.Select(e => e.ActionId).ToList(), "Actions assigned to role successfully.", System.Net.HttpStatusCode.OK);
+        }
+    }
+
+}
--- StayHub.Application/CQRS/RBAC/Command/UserRole/AssignRoleToUserCommand.cs
+++ StayHub.Application/CQRS/RBAC/Command/UserRole/AssignRoleToUserCommand.cs
@@ -6,27 +6,25 @@ using StayHub.Domain.Entity.RBAC;
 namespace StayHub.Application.CQRS.RBAC.Command.UserRole
 {
     // Include properties to be used as input for the command
-    public record AssignRoleToUserCommand(int UserId, int RoleId) : IRequest<BaseResponse<bool>>;
-    public sealed class AssignRoleToUserCommandHandler(IUserRepository userRepository,IUserRoleRepository userRoleRepository) : BaseResponseHandler, IRequestHandler<AssignRoleToUserCommand, BaseResponse<bool>>
+    public record AssignRoleToUserCommand(int UserId, List<int> RoleId) : IRequest<BaseResponse<List<int>>>;
+    public sealed class AssignRoleToUserCommandHandler(IUserRepository userRepository, IUserRoleRepository userRoleRepository) : BaseResponseHandler, IRequestHandler<AssignRoleToUserCommand, BaseResponse<List<int>>>
     {
-        public async Task<BaseResponse<bool>> Handle(AssignRoleToUserCommand request, CancellationToken cancellationToken)
+        public async Task<BaseResponse<List<int>>> Handle(AssignRoleToUserCommand request, CancellationToken cancellationToken)
         {
             var user = await userRepository.GetEntityByIdAsync(request.UserId);
             if (user == null)
             {
-                return Failure<bool>(message: "No user found", code: System.Net.HttpStatusCode.BadRequest);
+                return Failure<List<int>>(message: "No user found", code: System.Net.HttpStatusCode.BadRequest);
             }
-            if ( await userRoleRepository.ExistsAsync(e=>e.UserId ==request.UserId && e.RoleId==request.RoleId))
-            {
-
-                return Failure<bool>(message: "Role already assigned to user", code: System.Net.HttpStatusCode.BadRequest);
-            }
-            await userRoleRepository.AddAsync(new Domain.Entity.RBAC.UserRole
+            var existRoles = (await userRoleRepository.GetManyAsync(e => e.UserId == request.UserId && request.RoleId.Contains(e.RoleId),
+                selector: (e, i) => e.RoleId)).ToList();
+            var newRoles = request.RoleId.Except(existRoles).ToList();
+            var result = await userRoleRepository.AddRangeAsync(newRoles.Select(rid => new Domain.Entity.RBAC.UserRole
             {
                 UserId = request.UserId,
-                RoleId = request.RoleId
-            });
-            return Success<bool>(data: true,message:"Role assigned to user successfully");
+                RoleId = rid
+            }).ToList());
+            return Success<List<int>>(data: result.Select(e => e.RoleId).ToList(), message: "Role assigned to user successfully");
         }
     }
 
