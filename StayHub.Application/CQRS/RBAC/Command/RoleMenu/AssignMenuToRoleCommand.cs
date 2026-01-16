cab074d6ed82151f8d56523b2a64eb2b0c92e6a4
PHONGPC\minhp <minhphonghp2003@gmail.com>
2026-01-16 22:48:21 +0700

    ....

--- StayHub.Application/CQRS/RBAC/Command/RoleMenu/AssignMenuToRoleCommand.cs
+++ StayHub.Application/CQRS/RBAC/Command/RoleMenu/AssignMenuToRoleCommand.cs
@@ -11,12 +11,12 @@ namespace StayHub.Application.CQRS.RBAC.Command.RoleMenu
 {
     // Include properties to be used as input for the command
     public record AssignMenuToRoleCommand(List<int> menuIds, int roleId) : IRequest<Response<List<int>>>;
-    public sealed class AssignMenuToRoleCommandHandler(IRoleActionRepository roleActionRepository,IMenuActionRepository menuActionRepository) : BaseResponseHandler, IRequestHandler<AssignMenuToRoleCommand, Response<List<int>>>
+    public sealed class AssignMenuToRoleCommandHandler(IRoleActionRepository roleActionRepository,IMenuActionRepository menuActionRepository,IActionRepository actionRepository) : BaseResponseHandler, IRequestHandler<AssignMenuToRoleCommand, Response<List<int>>>
     {
         public async Task<Response<List<int>>> Handle(AssignMenuToRoleCommand request, CancellationToken cancellationToken)
         {
 
-            var actions = (await menuActionRepository.GetManyAsync(filter:e=>request.menuIds.Contains(e.MenuId),selector:(e,i)=>e.ActionId)).ToList();
+            var actions = (await actionRepository.GetManyAsync(filter:e=>e.MenuActions.Any(ma=>request.menuIds.Contains(ma.MenuId)),selector:(e,i)=>e.Id)).Distinct().ToList();
             await roleActionRepository.DeleteWhere(e => e.RoleId == request.roleId, false);
             var result = await roleActionRepository.AddRangeAsync(actions.Select(aid => new StayHub.Domain.Entity.RBAC.RoleAction
             {
