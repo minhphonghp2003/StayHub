82b949ed96cfbf93cf2a9bea29cb51c7ff19b77e
Minh Phong <minhphonghp2003@gmail.com>
2026-02-07 21:24:29 +0700

    update profile

--- StayHub.Application/CQRS/RBAC/Command/User/UpdateUserCommand.cs
+++ StayHub.Application/CQRS/RBAC/Command/User/UpdateUserCommand.cs
@@ -2,10 +2,6 @@
 using Shared.Response;
 using StayHub.Application.DTO.RBAC;
 using StayHub.Application.Interfaces.Repository.RBAC;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
@@ -15,29 +11,50 @@ namespace StayHub.Application.CQRS.RBAC.Command.User
     {
         [JsonIgnore]
         public int Id { get; set; }
+        public string Fullname { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+        public string? Image { get; set; }
 
-        public UpdateUserCommand(int id, string email)
+        public UpdateUserCommand(int id, string fullname, string email, string? phone = null, string? address = null, string? image = null)
         {
             Id = id;
+            Fullname = fullname;
             Email = email;
+            Phone = phone;
+            Address = address;
+            Image = image;
         }
 
         // Parameterless constructor for model binding / deserialization
         public UpdateUserCommand() { }
     }
-    public sealed class UpdateUserCommandCommandHandler(IUserRepository menuRepository) : BaseResponseHandler, IRequestHandler<UpdateUserCommand, BaseResponse<ProfileDTO>>
+    public sealed class UpdateUserCommandCommandHandler(IUserRepository userRepository) : BaseResponseHandler, IRequestHandler<UpdateUserCommand, BaseResponse<ProfileDTO>>
     {
         public async Task<BaseResponse<ProfileDTO>> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
         {
-            var user = new StayHub.Domain.Entity.RBAC.User
+            var user = await  userRepository.GetEntityByIdAsync(request.Id);
+            if (user == null)
             {
-                Id = request.Id,
-            };
-            menuRepository.Update(user);
+                return Failure<ProfileDTO>("User not found", System.Net.HttpStatusCode.NotFound);
+            }
+            user.Profile.Fullname =  request.Fullname;
+            user.Profile.Email = request.Email;
+            user.Profile.Phone = request.Phone;
+            user.Profile.Address = request.Address;
+            user.Profile.Image = request.Image;
+            userRepository.Update(user);
+            
             return Success<ProfileDTO>(new ProfileDTO
             {
                 Id = user.Id,
+                Username = user.Username,
+                Fullname = user.Profile.Fullname,
+                Email = user.Profile.Email,
+                Phone = user.Profile.Phone,
+                Address = user.Profile.Address,
+                Image = user.Profile.Image,
                 CreatedAt = user.CreatedAt,
                 UpdatedAt = user.UpdatedAt
             });
