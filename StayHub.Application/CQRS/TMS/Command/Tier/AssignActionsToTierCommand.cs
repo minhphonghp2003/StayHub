49f5e965feb2e35ae8f3375c3beb539b34769cbe
Minh Phong <minhphonghp2003@gmail.com>
2026-02-23 08:23:35 +0700

    assign actioio/menu to tier

---
+++ StayHub.Application/CQRS/TMS/Command/Tier/AssignActionsToTierCommand.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Shared.Response;
+using StayHub.Application.DTO.RBAC;
+using StayHub.Application.Interfaces.Repository.RBAC;
+using StayHub.Application.Interfaces.Repository.TMS;
+
+namespace StayHub.Application.CQRS.RBAC.Command.MenuAction
+{
+    // Include properties to be used as input for the command
+    public record AssignActionsToTierCommand(List<int> actionIds, int tierId) : IRequest<Response<List<int>>>;
+    public sealed class AssignActionsToTierCommandHandler(ITierRepository tierRepository,IActionRepository actionRepository) : BaseResponseHandler, IRequestHandler<AssignActionsToTierCommand, Response<List<int>>>
+    {
+        public async Task<Response<List<int>>> Handle(AssignActionsToTierCommand request, CancellationToken cancellationToken)
+        {
+           var tier = await tierRepository.FindOneEntityAsync(e=>e.Id==request.tierId,include:e=>e.Include(j=>j.Actions));
+            if(tier == null)
+                return Failure<List<int>>("Tier not found.", System.Net.HttpStatusCode.NotFound);
+            tier.Actions?.Clear();
+            tier.Actions = (await actionRepository.GetManyEntityAsync(e=>request.actionIds.Contains(e.Id))).ToList();
+            tierRepository.Update(tier);
+
+            return Success(request.actionIds, "Actions assigned to tier successfully.", System.Net.HttpStatusCode.OK);
+        }
+    }
+
+}
---
+++ StayHub.Application/CQRS/TMS/Command/Tier/AssignMenuToTierCommand.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Shared.Response;
+using StayHub.Application.Interfaces.Repository.RBAC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StayHub.Application.Interfaces.Repository.TMS;
+
+namespace StayHub.Application.CQRS.RBAC.Command.RoleMenu
+{
+    // Include properties to be used as input for the command
+    public record AssignMenuToTierCommand(List<int> menuIds, int tierId) : IRequest<Response<List<int>>>;
+    public sealed class AssignMenuToTierCommandHandler(ITierRepository tierRepository, IActionRepository actionRepository) : BaseResponseHandler, IRequestHandler<AssignMenuToTierCommand, Response<List<int>>>
+    {
+        public async Task<Response<List<int>>> Handle(AssignMenuToTierCommand request, CancellationToken cancellationToken)
+        {
+
+            var actions = (await actionRepository.GetManyAsync(filter:e=>e.MenuActions.Any(ma=>request.menuIds.Contains(ma.MenuId)),selector:(e,i)=>e)).Distinct().ToList();
+            var tier = await tierRepository.FindOneEntityAsync(e=>e.Id==request.tierId,include:e=>e.Include(j=>j.Actions));
+            if(tier == null)
+                return Failure<List<int>>("Tier not found.", System.Net.HttpStatusCode.NotFound);
+            tier.Actions?.Clear();
+            tier.Actions =  actions;
+            return Success(request.menuIds, "Menus assigned to tier successfully.");
+        }
+    }
+}
