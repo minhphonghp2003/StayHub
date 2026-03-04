3629b16b4bf48ad96d010e6333e26ff16dc1c3ce
PHONGPC\minhp <minhphonghp2003@gmail.com>
2026-03-04 22:50:55 +0700

    ..

---
+++ StayHub.Domain/Entity/PMM/Asset.cs
@@ -0,0 +1,6 @@
+using Shared.Common;
+namespace StayHub.Domain.Entity.PMM;
+public class Asset : BaseEntity 
+{ 
+    public string Name { get; set; } = string.Empty;
+}
---
+++ StayHub.Domain/Entity/PMM/Job.cs
@@ -0,0 +1,6 @@
+using Shared.Common;
+namespace StayHub.Domain.Entity.PMM;
+public class Job : BaseEntity 
+{ 
+    public string Name { get; set; } = string.Empty;
+}
