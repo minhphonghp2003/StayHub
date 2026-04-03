0c1b9e8ce10c6607251de9893e17d1a388a73ace
PHONGPC\minhp <minhphonghp2003@gmail.com>
2026-04-03 07:47:09 +0700

    noti kafka

--- NotificationService/Notification.API/Program.cs
+++ NotificationService/Notification.API/Program.cs
@@ -1,3 +1,5 @@
+using Notification.API;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAPIDI(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ NotificationService/Notification.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,39 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Notification.Application;
+using Notification.Infrastructure.Consumer;
+
+namespace Notification.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfraDI(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddAppDI(configuration);
+            service.AddMassTransit(x =>
+            {
+                const string fileExportedTopic = "file-exported";
+                const string kafkaBrokerServers = "localhost:9092";
+                x.UsingInMemory((context, cfg) => { cfg.ConfigureEndpoints(context); });
+
+                x.AddRider(rider =>
+                {
+                    rider.AddConsumer<FileExportedConsumer>();
+                    rider.UsingKafka((context, k) =>
+                    {
+                        k.Host(kafkaBrokerServers);
+
+                        k.TopicEndpoint<FileExportedEvent>(fileExportedTopic, "file-export-noti", e =>
+                        {
+                            e.ConfigureConsumer<FileExportedConsumer>(context);
+                            e.CreateIfMissing();
+                            e.AutoOffsetReset = Confluent.Kafka.AutoOffsetReset.Earliest;
+                        });
+                    });
+                });
+            });
+            return service;
+        }
+    }
+}
