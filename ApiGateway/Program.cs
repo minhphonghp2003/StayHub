cd6a5a8737efa2890663673cd04d788e614e53fe
PHONGPC\minhp <minhphonghp2003@gmail.com>
2026-04-05 16:56:14 +0700

    signalr endpoint

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,12 +1,52 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+using Yarp.ReverseProxy.Transforms;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+          .AddJwtBearer(options =>
+          {
+              options.TokenValidationParameters = new TokenValidationParameters
+              {
+                  ValidateIssuer = true, // Ensure the token was issued by a trusted issuer
+                  ValidIssuer = builder.Configuration["Jwt:Issuer"], // The expected issuer value from configuration
+                  ValidateAudience = false, // Disable audience validation (can be enabled as needed)
+                  ValidateLifetime = true, // Ensure the token has not expiredAuthService
+                  ValidateIssuerSigningKey = true, // Ensure the token's signing key is valid
+                  ClockSkew = TimeSpan.Zero,
+                  IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
 
+              };
+          });
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("authPolicy", policy => policy.RequireAuthenticatedUser());
+});
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy")).AddTransforms(transformContext =>
+    {
+        transformContext.AddRequestTransform(async context =>
+        {
+            var user = context.HttpContext.User;
+            if (user.Identity?.IsAuthenticated == true)
+            {
+                var userId = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                context.ProxyRequest.Headers.Add("X-User-Id", userId);
+            }
+        });
+    });
+;
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,7 +58,8 @@ if (app.Environment.IsDevelopment())
 
 //app.UseHttpsRedirection();
 
-//app.UseAuthorization();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapReverseProxy();
 
